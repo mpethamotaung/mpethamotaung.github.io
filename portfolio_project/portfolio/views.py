751dda3fbb77ec1190c407b18a00c91179032c44
Hanma-yujiro <mpethamotaung@gmail.com>
2023-08-15 12:06:46 +0200

    python views creation

--- portfolio_project/portfolio/views.py
+++ portfolio_project/portfolio/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
+def home(request):
+    return render(request, 'portfolio/home.html')
+
+def projets(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolio/projects.html', {'projects':projects})
