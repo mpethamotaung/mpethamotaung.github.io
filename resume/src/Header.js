40489e3077bb9d7dafadbb3383574d8808f02674
Hanma-yujiro <mpethamotaung@gmail.com>
2023-08-21 21:20:57 +0200

    porfolio header

---
+++ resume/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return(
+    <header>
+      <h1>Mpetha S. Motaung</h1>
+      <p>Software Engineer</p>
+    </header>
+  );
+}
+
+export default Header;
