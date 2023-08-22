de874b2c251960c9b18637653d08f814605ac124
Hanma-yujiro <mpethamotaung@gmail.com>
2023-08-22 21:18:39 +0200

    adding scroll-top js function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+/*Header functionality */
 document.querySelector('header').addEventListener('click', function() {
     this.style.backgroundColor = '#f4f4f4'
 })
+
+/*Scroll button functionality */
+document.addEventListener('DOMContentLoaded', function() {
+    const scrollToTopButton = document.getElementById('scroll-to-top');
+
+    scrollToTopButton.addEventListener('click', function () {
+        window.scrollTo({
+            top:0,
+            behavior:'smooth'
+        });
+    });
+
+    window.addEventListener('scroll', function () {
+        if (this.window.scrollY > 300){
+            scrollToTopButton.classList.add('active');
+        }else{
+            scrollToTopButton.classList.remove('active');
+        }
+    });
+});
