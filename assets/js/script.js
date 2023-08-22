da3a5de196cd9a406920d977fdb0c91576b9489d
Hanma-yujiro <mpethamotaung@gmail.com>
2023-08-22 21:53:53 +0200

    burger js

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,13 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+/*Navbar Toggle Function*/
+document.addEventListener('DOMContentLoaded', function () {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbarCollapse = document.querySelector('.navbar-collapse')
+
+    navbarToggler.addEventListener('click', function () {
+        navbarCollapse.classList.toggle('show');
+    })
+})
