587ed3ea6423155d80275fb0ec7621efec090e3b
Hanma-yujiro <mpethamotaung@gmail.com>
2024-06-26 12:14:25 +0200

    rollback

--- assets/js/scripts2.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('navbar-toggler').addEventListener('click', function() {
-    var menu = document.getElementById('navbar-menu');
-    if (menu.classList.contains('show')) {
-        menu.classList.remove('show');
-    } else {
-        menu.classList.add('show');
-    }
-});
